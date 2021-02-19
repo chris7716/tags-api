b184b7a8fe368192f5e8b1fce9009ab5ad84a3b2
Hasitha Kaushan <hkraclh@gmail.com>
2021-02-19 16:58:39 +0530

    new settings configured

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require("express");
 const app = express();
 const { ValidationError } = require('express-validation')
 const customerRouter = require("./api/customers/customer.router");
+const systemRouter = require("./api/systems/system.router");
+const tagRouter = require("./api/tags/tag.router");
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 app.use("/api/customer", customerRouter);
+app.use("/api/system", systemRouter);
+app.use("/api/tag", tagRouter);
 app.use(function(err, req, res, next) {
     if (err instanceof ValidationError) {
       return res.status(err.statusCode).json(err);
