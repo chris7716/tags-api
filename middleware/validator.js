9258d484d3396ef696b34f42718572b6a900c481
Hasitha Kaushan <hkraclh@gmail.com>
2021-02-19 16:57:09 +0530

    validations added

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,4 +1,5 @@
 const { Joi } = require('express-validation');
+
 const createCustomerValidation = {
   query: Joi.object({
     name: Joi.string()
@@ -10,7 +11,54 @@ const createCustomerValidation = {
       .required(),
   }),
 };
+const createSystemValidation = {
+  query: Joi.object({
+    customerId: Joi.string()
+      .required(),
+    name: Joi.string()
+      .required(),
+    description: Joi.string()
+      .allow(),
+  }),
+};
+const listSystemByCustomerValidation = {
+  query: Joi.object({
+    customerId: Joi.string()
+      .required(),
+  }),
+};
+const createTagValidation = {
+  query: Joi.object({
+    systemId: Joi.string()
+      .required(),
+    name: Joi.string()
+      .required(),
+    description: Joi.string()
+      .allow(),
+  }),
+};
+const createTagValueValidation = {
+  query: Joi.object({
+    tagId: Joi.string()
+      .required(),
+    value: Joi.string()
+      .allow(),
+  }),
+};
+const getLastTagValueValidation = {
+  query: Joi.object({
+    tagId: Joi.string()
+      .required(),
+    value: Joi.string()
+      .allow(),
+  }),
+};
 
 module.exports = {
-  createCustomerValidation
+  createCustomerValidation,
+  createSystemValidation,
+  listSystemByCustomerValidation,
+  createTagValidation,
+  createTagValueValidation,
+  getLastTagValueValidation,
 };
